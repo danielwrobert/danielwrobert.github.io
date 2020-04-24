5cb6be0bd6033e66507ce5f9e21cb5f229ce3bc6
Daniel W. Robert <danielwrobert@gmail.com>
2020-04-24 12:11:18 -0400

    Added global styles to layout component.

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,7 +2,8 @@ import React from 'react';
 import Header from './header';
 import Footer from './footer';
 
-// import '../styles/global.css';
+import '../styles/normalize.css';
+import '../styles/global.css';
 
 export default ({ children }) => (
 	<div>
